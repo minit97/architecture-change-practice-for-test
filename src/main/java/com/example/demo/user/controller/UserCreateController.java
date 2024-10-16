b4764ad84f4821834d75c2b923597e98650ab329
minit <dndb4902@naver.com>
2024-10-16 11:23:02 +0900

    refator: 도메인과 영속성 객체 구분

--- src/main/java/com/example/demo/user/controller/UserCreateController.java
+++ src/main/java/com/example/demo/user/controller/UserCreateController.java
@@ -1,8 +1,8 @@
 package com.example.demo.user.controller;
 
+import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserCreate;
 import com.example.demo.user.controller.response.UserResponse;
-import com.example.demo.user.infrastructure.UserEntity;
 import com.example.demo.user.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -18,16 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserCreateController {
-
-    private final UserController userController;
     private final UserService userService;
 
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@RequestBody UserCreate userCreateDto) {
-        UserEntity userEntity = userService.create(userCreateDto);
+        User user = userService.create(userCreateDto);
         return ResponseEntity
             .status(HttpStatus.CREATED)
-            .body(userController.toResponse(userEntity));
+            .body(UserResponse.from(user));
     }
 
 }
