eedd26937d511c73331903cda8c490dbfb731ab7
minit <dndb4902@naver.com>
2024-10-16 09:46:28 +0900

    refator: MailSender 의존성 역전 적용

--- src/main/java/com/example/demo/user/service/UserService.java
+++ src/main/java/com/example/demo/user/service/UserService.java
@@ -22,7 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final JavaMailSender mailSender;
+    private final CertificationService certificationService;
 
 
     public UserEntity getByEmail(String email) {
@@ -44,8 +44,7 @@ public class UserService {
         userEntity.setStatus(UserStatus.PENDING);
         userEntity.setCertificationCode(UUID.randomUUID().toString());
         userEntity = userRepository.save(userEntity);
-        String certificationUrl = generateCertificationUrl(userEntity);
-        sendCertificationEmail(userCreateDto.getEmail(), certificationUrl);
+        certificationService.sendCertificationEmail(userCreateDto.getEmail(), userEntity.getId(), userEntity.getCertificationCode());
         return userEntity;
     }
 
@@ -71,17 +70,5 @@ public class UserService {
             throw new CertificationCodeNotMatchedException();
         }
         userEntity.setStatus(UserStatus.ACTIVE);
-    }
-
-    private void sendCertificationEmail(String email, String certificationUrl) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject("Please certify your email address");
-        message.setText("Please click the following link to certify your email address: " + certificationUrl);
-        mailSender.send(message);
-    }
-
-    private String generateCertificationUrl(UserEntity userEntity) {
-        return "http://localhost:8080/api/users/" + userEntity.getId() + "/verify?certificationCode=" + userEntity.getCertificationCode();
     }
 }
